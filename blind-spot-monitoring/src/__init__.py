571b521ea700d977c935dc6cb7a689bbaa146284
Recep Yalçınkaya <137687914+recepyalcinkaya@users.noreply.github.com>
2026-03-30 13:28:23 +0300

    Initialize blind-spot-monitoring module with imports

---
+++ blind-spot-monitoring/src/__init__.py
@@ -0,0 +1,7 @@
+from .sensor_reader import DistanceSensorReader
+from .alert_system import BlindSpotAlerter
+from .led_controller import MirrorLED
+
+__version__ = "1.1.0"
+__author__ = "Recep Yalcinkaya"
+__all__ = ["DistanceSensorReader", "BlindSpotAlerter", "MirrorLED"]
